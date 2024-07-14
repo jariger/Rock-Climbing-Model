9f6909a8ed6e51dd625f937247228b300a94e7cc
jariger <jariger2004@gmail.com>
2024-07-14 14:56:07 -0400

    gitignore
    commit

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,13 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# Create or switch to gh-pages branch
+git branch -M gh-pages
+
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jariger/Rock-Climbing-Model.git main:gh-pages
+git push -f git@github.com:jariger/Rock-Climbing-Model.git gh-pages
 
 cd -
